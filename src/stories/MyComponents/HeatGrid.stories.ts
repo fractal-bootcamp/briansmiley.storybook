f5d9c1a0cf32a4eb8b1ebbcbcf77a1c3642531d3
Brian Smiley <briantsmiley42@gmail.com>
2024-06-05 15:59:01 -0400

    removes unneeded type import

--- src/stories/MyComponents/HeatGrid.stories.ts
+++ src/stories/MyComponents/HeatGrid.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import HeatGrid, { HeatGridProps } from './HeatGrid'
+import HeatGrid from './HeatGrid'
 
 const meta: Meta<typeof HeatGrid> = {
     title: 'Heat Grid',
