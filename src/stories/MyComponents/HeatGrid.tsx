89a8ed0c4f58ee1049f9fceaa9df48058e4c9ee0
Brian Smiley <briantsmiley42@gmail.com>
2024-06-06 18:05:49 -0400

    refactors component mockProps and makes App.tsx run a component previewer

--- src/stories/MyComponents/HeatGrid.tsx
+++ src/stories/MyComponents/HeatGrid.tsx
@@ -1,63 +1,82 @@
 import "../../index.css";
 
 export type HeatGridProps = {
-    gridValues: number[][]
-}
+  gridValues: number[][];
+};
 
 type RGBObj = {
-    red: number,
-    green: number,
-    blue: number,
-}
+  red: number;
+  green: number;
+  blue: number;
+};
 type GridCellProps = {
-    value: number
-}
+  value: number;
+};
+export const mockHeatGrid = Array(5)
+  .fill(0)
+  .map(() => Array.from({ length: 7 }, () => Math.round(Math.random() * 100)));
 
 //Returns the color at the midpoint of startColor and endColor in RGB space, default assumes value is a percentage
-function interpolatedColor(startColor: RGBObj, endColor: RGBObj, value: number, min: number = 0, max: number = 100) {
-    const rangeLimit = (num: number):number => Math.min(255, Math.max(num, 0))
-    const red = startColor.red + (endColor.red - startColor.red)*(value - min) / (max - min);
-    const blue = startColor.blue + (endColor.blue - startColor.blue)*(value - min) / (max - min);
-    const green = startColor.green + (endColor.green - startColor.green)*(value - min) / (max - min);
-    const colorCode = "#" + [red, green, blue].map( num => Math.round(rangeLimit(num)).toString(16) ).join("")
-    return colorCode
+function interpolatedColor(
+  startColor: RGBObj,
+  endColor: RGBObj,
+  value: number,
+  min: number = 0,
+  max: number = 100
+) {
+  const rangeLimit = (num: number): number => Math.min(255, Math.max(num, 0));
+  const red =
+    startColor.red +
+    ((endColor.red - startColor.red) * (value - min)) / (max - min);
+  const blue =
+    startColor.blue +
+    ((endColor.blue - startColor.blue) * (value - min)) / (max - min);
+  const green =
+    startColor.green +
+    ((endColor.green - startColor.green) * (value - min)) / (max - min);
+  const colorCode =
+    "#" +
+    [red, green, blue]
+      .map(num => Math.round(rangeLimit(num)).toString(16))
+      .join("");
+  return colorCode;
 }
-const GridCell = ({value}: GridCellProps ) => {
-    const startColor = {red: 217, green:217, blue:217}
-    const endColor = {red: 62, green:95, blue:204}
-    const cellColor = interpolatedColor(startColor, endColor, value)
-    const cellColorStyle = { background: cellColor }
-    return (
-        <div className="flex" >
-           <button className="w-[60px] h-[60px]" style={cellColorStyle}></button>
-        </div>
-    )
-}
-const HeatGrid= ({gridValues}: HeatGridProps) => {
-    const days = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
-    
-    return(
-        <div className="flex flex-col gap-3">
-            <div className="flex flex-row gap-3">
-                {days.map( day => {
-                    return (
-                        <div className="flex w-[60px] h-[60px] justify-center items-end">
-                            {day}
-                        </div>
-                    )
-                })}
-            </div>
-            {gridValues.map( row => {
-                return (
-                    <div className="flex flex-row gap-3">
-                        {row.map( value => (
-                            <GridCell value={value}/>
-                        ))}
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+const GridCell = ({ value }: GridCellProps) => {
+  const startColor = { red: 217, green: 217, blue: 217 };
+  const endColor = { red: 62, green: 95, blue: 204 };
+  const cellColor = interpolatedColor(startColor, endColor, value);
+  const cellColorStyle = { background: cellColor };
+  return (
+    <div className="flex">
+      <button className="w-[60px] h-[60px]" style={cellColorStyle}></button>
+    </div>
+  );
+};
+const HeatGrid = ({ gridValues }: HeatGridProps) => {
+  const days = ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"];
 
-export default HeatGrid
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex flex-row gap-3">
+        {days.map(day => {
+          return (
+            <div className="flex w-[60px] h-[60px] justify-center items-end">
+              {day}
+            </div>
+          );
+        })}
+      </div>
+      {gridValues.map(row => {
+        return (
+          <div className="flex flex-row gap-3">
+            {row.map(value => (
+              <GridCell value={value} />
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HeatGrid;
--- src/stories/MyComponents/Tasklist.tsx
+++ src/stories/MyComponents/Tasklist.tsx
@@ -5,7 +5,35 @@ import { useState } from "react";
 export type TaskListProps = {
   tasks: Omit<TaskProps, "toggleFunction">[];
 };
-
+export const mockTasks: TaskListProps = {
+  tasks: [
+    {
+      name: "first task",
+      id: "1",
+      description: "do stuff",
+      completed: false
+    },
+    {
+      name: "second task",
+      id: "2",
+      description: "do more stuff",
+      completed: true
+    },
+    {
+      name: "Sweep the Kitchen",
+      id: "3",
+      description: "Get under the cabinets, do a good job",
+      completed: false
+    },
+    {
+      name: "Walk the dog to the park before noon",
+      id: "4",
+      description:
+        "bow wowbow wowbow wowbow wowbow wowbow wowbow wowbow wowbow wow",
+      completed: true
+    }
+  ]
+};
 export const TaskList = ({ tasks }: TaskListProps) => {
   const [taskList, setTaskList] = useState(tasks);
   // const toggleCompleted = (t: TaskProps) => () => {
@@ -24,21 +52,21 @@ export const TaskList = ({ tasks }: TaskListProps) => {
 
     const sortedTaskList = sortByCompletion(newTaskList);
     setTaskList(sortedTaskList);
-};
+  };
 
-const sortByCompletion = (list: Omit<TaskProps, "toggleFunction">[]) => {
+  const sortByCompletion = (list: Omit<TaskProps, "toggleFunction">[]) => {
     const completedTasks = list.filter(task => task.completed);
     const uncompletedTasks = list.filter(task => !task.completed);
-    return uncompletedTasks.concat(completedTasks)
-}
+    return uncompletedTasks.concat(completedTasks);
+  };
 
   return (
     <div className="flex flex-col gap-4">
       {taskList.map(task => (
         <Task
-        {...task}
-        key={task.id}
-        toggleFunction={() => toggleCompleted(task.id)}
+          {...task}
+          key={task.id}
+          toggleFunction={() => toggleCompleted(task.id)}
         />
       ))}
     </div>
